9c453c1da1d7c96d8fd974da685bfe4a4ca4397c
justaguy12345 <ryanberk135@gmail.com>
2022-08-20 19:21:30 -0500

    changed title to key

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use serde_json::{Value};
 use std::{path::Path, fs::{File}, io::{Read, Write}, collections::{HashMap}, process::{exit}};
 
 use crate::Operations::{ADD};
+#[derive(Clone)]
 struct Json_Structures
 {
     outer: String,
@@ -38,16 +39,18 @@ fn main()
     let filename = "testjson";
     let ext = ".json";
     let full_filename = filename.to_owned() + ext;
-    let mut json_block  = get_file(full_filename.clone());
-    //json_block = init_json(jstructure);
-/*    json_block = make_first_entry(
-        json_block.clone().to_string(), "title0".to_string(), "data".to_string(), "6".to_string()
+    let mut json_block: String;
+    json_block  = get_file(full_filename.clone());
+   /* json_block = init_json(jstructure.clone());
+    json_block = make_first_entry(
+        json_block.clone().to_string(), "key0".to_string(), "data".to_string(), "6".to_string()
     );
     json_block = make_entry(
-        json_block.clone().to_string(), "title1".to_string(), "data".to_string(), "6".to_string(), jstructure
+        json_block.clone().to_string(), "key1".to_string(), "data".to_string(), "6".to_string(), jstructure
     );*/
-    json_block = update_entry(json_block.clone(), "title1".to_string(), "data".to_string(), jstructure, ADD, 1);
-    overwrite_data(json_block, full_filename);
+    json_block = update_entry(json_block.clone(), "key1".to_string(), "data".to_string(), jstructure, ADD, 1);
+    overwrite_data(json_block.clone(), full_filename);
+    dbg!(json_block.clone());
 }
 
 fn overwrite_data(json_block: String, full_filename: String)
@@ -81,29 +84,29 @@ fn init_json( jstructure: Json_Structures ) -> String
                                                            //the top
     outer_structure.insert_str(3 , &middle_structure);
     let json_block = outer_structure.clone();
-    dbg!(&json_block);
+    //dbg!(&json_block);
     return json_block;
 }
 
-fn make_first_entry(mut json_block: String, title: String, data:  String, n: String) -> String
+fn make_first_entry(mut json_block: String, key: String, data:  String, n: String) -> String
 {
-    let t_len = title.len();
+    let t_len = key.len();
     let d_len = data.len();
-    json_block.insert_str(5 ,&title);//only accounting for starting block aka outer structure
-    json_block.insert_str(13+t_len ,&data);//accounting for title added in this step
-    json_block.insert_str(16+d_len+t_len ,&n);//accounting for title and data here
+    json_block.insert_str(5 ,&key);//only accounting for starting block aka outer structure
+    json_block.insert_str(13+t_len ,&data);//accounting for key added in this step
+    json_block.insert_str(16+d_len+t_len ,&n);//accounting for key and data here
     return json_block;
 }
 
-fn make_entry(mut json_block: String, title: String, data:  String, n: String, jstructure: Json_Structures) -> String
+fn make_entry(mut json_block: String, key: String, data:  String, n: String, jstructure: Json_Structures) -> String
 {
-    let t_len = title.len();
+    let t_len = key.len();
     let d_len = data.len();
     let mut new_inner_structure = jstructure.new_inner;
     let mut middle_structure = jstructure.middle;
     new_inner_structure.insert_str(6, &data);
     new_inner_structure.insert_str(9+d_len, &n);
-    middle_structure.insert_str(2, &title);
+    middle_structure.insert_str(2, &key);
     middle_structure.insert_str(4+t_len, &new_inner_structure);
     json_block.insert_str(3, &middle_structure);
     return json_block;    
@@ -111,7 +114,7 @@ fn make_entry(mut json_block: String, title: String, data:  String, n: String, j
 
 fn update_entry
 (
-    mut json_block: String, title: String, data:  String, jstructure: Json_Structures, opt: Operations, 
+    mut json_block: String, key: String, data:  String, jstructure: Json_Structures, opt: Operations, 
     opt_val: i64
 ) -> String
 {
@@ -123,7 +126,7 @@ fn update_entry
     for (x, y) in  &jObj.clone()[0]
     {
         i = i + 1;
-        if x == &title
+        if x == &key
         {
             old_value = y[0][data.clone()].as_i64().expect("error parsing data as i64");
             was_found = true;
@@ -143,11 +146,11 @@ fn update_entry
     {
         return old_data + opt_val;
     }
-    //let t_loc = json_block.find(&title).expect("cannot find Key");
+    //let t_loc = json_block.find(&key).expect("cannot find Key");
     let mut middle_structure = jstructure.middle.clone();
     let mut final_inner_structure = jstructure.final_inner.clone();
     let compare_model: String;
-    middle_structure.insert_str(2, &title);
+    middle_structure.insert_str(2, &key);
     final_inner_structure.insert_str(6, &data);
     final_inner_structure.insert_str(9+d_len, &old_value.to_string());
     middle_structure.insert_str(middle_structure.len(), &final_inner_structure);
@@ -155,12 +158,12 @@ fn update_entry
     middle_structure = jstructure.middle;
     final_inner_structure = jstructure.final_inner;
     let new_model: String;
-    middle_structure.insert_str(2, &title);
+    middle_structure.insert_str(2, &key);
     final_inner_structure.insert_str(6, &data);
     final_inner_structure.insert_str(9+d_len, &new_val.to_string());
     middle_structure.insert_str(middle_structure.len(), &final_inner_structure);
     new_model = middle_structure;
     json_block = json_block.replace(&compare_model, &new_model);
-    dbg!(&json_block);
+    //dbg!(&json_block);
     return json_block;
 }
